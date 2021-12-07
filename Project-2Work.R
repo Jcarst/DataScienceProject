49fb0e7c18092b64d4daf4ffb7f49aae0c5e7b5c
Jcarst <95450849+Jcarst@users.noreply.github.com>
2021-12-07 13:20:22 -0700

    Add files via upload
    12/07/21: Added plot codes to markdown file. Also wrote one paragraph.

--- Project-2Work.R
+++ Project-2Work.R
@@ -4,36 +4,9 @@ library(GGally)
 library(ggfortify)
 library(mdsr)
 
+
 data <- read.csv("Project3Sample4000.csv")
 
-############### Matt's code
-data <- data %>% select(!c(name, dob, c_jail_in, c_jail_out, RiskRecidScreeningDate, c_charge_desc))
-
-model <- glm(isRecid~.-c_charge_degree
-             -RiskViolenceScoreLevel
-             -ageCat
-             -RiskRecidScoreLevel
-             -RiskViolenceDecileScore
-             -juvMisdemeanerCount
-             -juvOtherCount
-             -c_days_from_compas
-             -race
-             -juvFelonyCount,data,family="binomial")
-summary(model)
-
-dataReduced <- data %>% select(sex,age,priorsCount,days_b_screening_arrest,RiskRecidDecileScore,isRecid)
-
-dataReduced$age <- log(dataReduced$age)
-model2 <- glm(isRecid~.,dataReduced,family="binomial")
-
-summary(model2)
-
-ggplot(data)
-
-
-
-
-############ Johnny's Code
 pairs <- data %>% select(!c(name,dob, c_jail_in, c_jail_out, RiskRecidScreeningDate,c_charge_desc))
 
 data$ageCat <- as.factor(data$ageCat)
