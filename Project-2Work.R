953f13619484a10c30452535a03f82bc0f33775f
Jcarst <95450849+Jcarst@users.noreply.github.com>
2021-12-09 15:33:32 -0700

    Add files via upload

--- Project-2Work.R
+++ Project-2Work.R
@@ -17,11 +17,11 @@ ggpairs(pairs)
 recidModel <- glm(isRecid ~., pairs, family = "binomial")
 model <- glm(isRecid ~.-ageCat, pairs, family = "binomial")
 
-ggplot(pairs, aes(x=race, fill=as.factor(isRecid), color = race, alpha = 0.5)) +  geom_density(aes(color = race))
 
-ggplot(pairs, aes(x=sex, fill = as.factor(isRecid), color = sex, alpha = 0.2)) + geom_density()
 
-ggplot(pairs, aes(x=priorsCount, y = isRecid, color = race)) + geom_point() + geom_jitter()
+ggplot(pairs, aes(x=sex, y = isRecid, color = sex, alpha = 0.2)) + geom_density()
+
+ggplot(pairs, aes(x=priorsCount, fill = as.factor(isRecid), alpha = 0.1)) + geom_density()
 
 priorsModel <- glm(isRecid ~ priorsCount, pairs, family = "binomial")
 summary(priorsModel)
@@ -29,5 +29,52 @@ summary(priorsModel)
 pairs$RiskRecidDecileScore <- log(pairs$RiskRecidDecileScore)
 ggpairs(pairs)
 
+regressionModel <- glm(isRecid ~ sex + priorsCount + age, pairs, family = "binomial")
+summary(regressionModel)
 
-       
+ageModel <- glm(isRecid ~ log(age), pairs, family = "binomial")
+summary(ageModel)
+
+
+### Multiple Regression ### 
+
+mrData <- pairs %>% 
+  select(!c(RiskViolenceScoreLevel, isRecid, ageCat, RiskRecidScoreLevel, RiskViolenceDecileScore, RiskViolenceScoreLevel, c_days_from_compas, days_b_screening_arrest)) %>% 
+  mutate(logAge = log10(age))
+
+RiskRecidDecileModel <- lm(RiskRecidDecileScore ~., mrData)
+
+forwardSelect <- ols_step_forward_p(RiskRecidDecileModel)
+forwardSelect
+
+backwardsSelect <- ols_step_backward_p(RiskRecidDecileModel)
+backwardsSelect
+
+bestSubset <- ols_step_best_subset(RiskRecidDecileModel)
+bestSubset # Pick 8 
+
+mixedSelection <- ols_step_both_p(RiskRecidDecileModel)
+mixedSelection
+
+bestModel <- lm(RiskRecidDecileScore ~ age + race + juvFelonyCount + juvOtherCount + priorsCount, mrData)
+summary(bestModel)
+trial <- ols_step_best_subset(bestModel)
+
+### Riskof Violence Decile Score ### 
+
+pleaseGodHelp <- pairs %>% 
+  select(!c(RiskViolenceScoreLevel, isRecid, ageCat, RiskRecidScoreLevel, RiskRecidDecileScore, RiskViolenceScoreLevel, c_days_from_compas, days_b_screening_arrest))
+
+byron <- lm(RiskViolenceDecileScore ~ ., pleaseGodHelp)
+
+chocolateChipIceCream <- ols_step_forward_p(byron)
+chocolateChipIceCream
+
+anime <- ols_step_backward_p(byron)
+anime 
+
+naruto <- ols_step_best_subset(byron)
+naruto
+
+initialD <- ols_step_both_p(byron) # Choose this one 
+initialD
